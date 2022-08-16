c96c9267438454e449f00c56c907a04ed28ad85e
apdkeb <apdkeb@naver.com>
2022-08-16 18:42:37 +0900

    feat : REST-Assured 추가
    2022. 08. 16.
    1. build.gradl
    - REST-Assured 추가
    
    2. AcceptanceTest
    - RestAssured를 사용하기 위한 세팅클래스
    - RestAssured를 이용하는 테스트 클래스는 상속받아 사용함.
    
    3. StudyTest
    - RestAssured를 이용하는 블랙박스 테스트
    
    Resolves : loop-study
    See also :

---
+++ src/test/java/loopstudy/restassured/acceptance/StudyTest.java
@@ -0,0 +1,46 @@
+package loopstudy.restassured.acceptance;
+
+import org.junit.jupiter.api.Test;
+
+public class StudyTest extends AcceptanceTest {
+
+    /**
+     * When 스터디 생성 요청을 하면
+     * Then 스터디 생성이 된다.
+     */
+    @Test
+    void 스터디_생성() {
+
+    }
+
+    /**
+     * Given 스터디 생성 요청을 하고
+     * When 스터디 조회 요청을 하면
+     * Then 스터디가 조회된다.
+     */
+    @Test
+    void 스터디_조회() {
+
+    }
+
+    /**
+     * Given 스터디 생성 요청을 하고
+     * When 스터디 정보 수정을 요청하면
+     * Then 스터디 정보가 수정된다.
+     */
+    @Test
+    void 스터디_수정() {
+
+    }
+
+
+    /**
+     * Given 스터디 생성 요청을 하고
+     * When 스터디 삭제 요청을 하면
+     * Then 스터디가 삭제된다.
+     */
+    @Test
+    void 스터디_삭제() {
+
+    }
+}
